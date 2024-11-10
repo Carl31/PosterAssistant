87cf92fe61a1573b5ed9d2b11154da07c3b067a8
Carl1131 <carlsantos31@yahoo.com>
2024-11-11 02:10:41 +1300

    Update app.js

--- backend/api/app.js
+++ backend/api/app.js
@@ -1,20 +1,3 @@
-const express = require('express');
-const connectDB = require('../db'); // import the db connection
-
-const app = express();
-
-// Connect to the database
-connectDB();
-
-app.get('/', (req, res) => {
-  res.send('Hello from Express!');
-});
-
-// Export the express app for Vercel
-module.exports = app;
-
-
-// Not needed as vercel expects serverless functions to export a request handler.
-// app.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`);
-// });
+module.exports = (req, res) => {
+  res.status(200).send('Hello from the Vercel serverless function!');
+};
